7866b8ca480918bc910782995a26ad9e88aac1ad
四季 <112454748+Bourbon1006@users.noreply.github.com>
2024-09-22 14:03:25 +0800

    Update TestMailClient.java

--- mail1/src/main/java/org/example/TestMailClient.java
+++ mail1/src/main/java/org/example/TestMailClient.java
@@ -7,13 +7,13 @@ public class TestMailClient {
     public static void main(String[] args) {
         String host = "smtp.qq.com";
         String port = "587";
-        String username = "2830446056@qq.com";
-        String password = "qoanyhhaencqdgae";
+        String username = "@qq.com";
+        String password = "";
 
         MailSender mailSender = new MailSender(host, port, username, password);
 
         try {
-            mailSender.sendEmail("burbon1006@gmail.com", "测试主题", "测试消息内容", "E:\\e\\javanetcode\\mail1\\src\\main\\java\\org\\example\\1.txt"); // 发送带附件的邮件
+            mailSender.sendEmail("@gmail.com", "测试主题", "测试消息内容", ""); // 发送带附件的邮件
             System.out.println("Email sent successfully.");
         } catch (MessagingException ex) {
             ex.printStackTrace();
